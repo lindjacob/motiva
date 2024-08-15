b80a769c046951c9430222b4fce301a119f97e77
lindjacob <jacob.lind2@gmail.com>
2024-08-15 18:37:58 +0200

    add create quote logic

--- app.vue
+++ app.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="container flex flex-col items-center justify-center h-screen">
+  <div class="flex flex-col items-center justify-center h-screen">
     <QuoteCard :quote="quotes[quoteNumber]" />
-    <div class="mt-16 flex justify-between gap-4">
+    <div class="mt-15 flex justify-between gap-4">
       <v-btn
         class="w-56"
         size="x-large"
         variant="outlined"
         prepend-icon="mdi-plus-thick"
-        @click="getNewQuote"
+        @click="showCreateQuoteModal"
       >
         Create quote
       </v-btn>
@@ -21,11 +21,50 @@
         New Quote
       </v-btn>
     </div>
+
+    <div
+      v-if="isModalVisible"
+      class="fixed inset-0 bg-white/90 flex justify-center items-center"
+    >
+      <div class="bg-white p-10 rounded-lg shadow-lg flex flex-col gap-4">
+        <v-textarea
+          v-model="newQuote"
+          color="#5ad796"
+          label="Quote"
+          rows="2"
+        ></v-textarea>
+        <v-text-field
+          v-model="newAuthor"
+          color="#5ad796"
+          label="Author"
+        ></v-text-field>
+        <div class="mt-4 flex justify-between gap-4">
+          <v-btn
+            class="w-56"
+            size="x-large"
+            variant="outlined"
+            @click="hideCreateQuoteModal"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            class="w-56"
+            size="x-large"
+            variant="flat"
+            color="#5ad796"
+            :disabled="!isSaveButtonActive"
+            @click="saveQuote"
+          >
+            Save
+          </v-btn>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted, onUnmounted } from "vue";
+import { ref, onMounted, onUnmounted, watch } from "vue";
 const { data: quotes } = await useFetch("https://zenquotes.io/api/quotes/");
 const quoteNumber = ref(Math.floor(Math.random() * quotes.value.length));
 
@@ -51,6 +90,33 @@ onMounted(() => {
 
 onUnmounted(() => {
   clearInterval(interval); // Clear interval when component is unmounted
+});
+
+const isModalVisible = ref(false);
+const newQuote = ref("");
+const newAuthor = ref("");
+const isSaveButtonActive = ref(false);
+
+const showCreateQuoteModal = () => {
+  isModalVisible.value = true;
+};
+
+const hideCreateQuoteModal = () => {
+  isModalVisible.value = false;
+  newQuote.value = "";
+  newAuthor.value = "";
+};
+
+const saveQuote = () => {
+  const savedQuotes = JSON.parse(localStorage.getItem("savedQuotes") || "[]");
+  savedQuotes.push({ q: newQuote.value, a: newAuthor.value });
+  localStorage.setItem("savedQuotes", JSON.stringify(savedQuotes));
+  hideCreateQuoteModal();
+};
+
+watch([newQuote, newAuthor], () => {
+  isSaveButtonActive.value =
+    newQuote.value.length >= 5 && newAuthor.value.length >= 2;
 });
 </script>
 
--- components/QuoteCard.vue
+++ components/QuoteCard.vue
@@ -1,7 +1,7 @@
 <template>
   <blockquote class="text-6xl font-bold text-[#5ad796] max-w-[1000px]">
     &ldquo; {{ quote.q }} &rdquo;
-    <footer class="mt-10 text-5xl text-black font-normal">
+    <footer class="mt-8 text-5xl text-black font-normal">
       <p>&mdash; {{ quote.a }}</p>
     </footer>
   </blockquote>
