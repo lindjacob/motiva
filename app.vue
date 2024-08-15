a1c319285abd5da7d455e7bd14bb1e856b544796
lindjacob <jacob.lind2@gmail.com>
2024-08-15 17:45:01 +0200

    add QuoteCard + api call + rotation logic

--- app.vue
+++ app.vue
@@ -1,6 +1,57 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="container flex flex-col items-center justify-center h-screen">
+    <QuoteCard :quote="quotes[quoteNumber]" />
+    <div class="mt-16 flex justify-between gap-4">
+      <v-btn
+        class="w-56"
+        size="x-large"
+        variant="outlined"
+        prepend-icon="mdi-plus-thick"
+        @click="getNewQuote"
+      >
+        Create quote
+      </v-btn>
+      <v-btn
+        class="w-56"
+        size="x-large"
+        variant="outlined"
+        append-icon="mdi-fast-forward"
+        @click="getNewQuote"
+      >
+        New Quote
+      </v-btn>
+    </div>
   </div>
 </template>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from "vue";
+const { data: quotes } = await useFetch("https://zenquotes.io/api/quotes/");
+const quoteNumber = ref(Math.floor(Math.random() * quotes.value.length));
+
+let interval;
+
+const getNewQuote = () => {
+  if (quoteNumber.value + 1 >= quotes.value.length) {
+    quoteNumber.value = 0;
+  } else {
+    quoteNumber.value++;
+  }
+  resetTimer();
+};
+
+const resetTimer = () => {
+  clearInterval(interval);
+  interval = setInterval(getNewQuote, 60000); // Change quote every 60 seconds
+};
+
+onMounted(() => {
+  interval = setInterval(getNewQuote, 60000); // Initial interval setup
+});
+
+onUnmounted(() => {
+  clearInterval(interval); // Clear interval when component is unmounted
+});
+</script>
+
+<style scoped></style>
---
+++ components/QuoteCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <blockquote class="text-6xl font-bold text-[#5ad796] max-w-[1000px]">
+    &ldquo; {{ quote.q }} &rdquo;
+    <footer class="mt-10 text-5xl text-black font-normal">
+      <p>&mdash; {{ quote.a }}</p>
+    </footer>
+  </blockquote>
+</template>
+
+<script setup>
+const { quote } = defineProps(["quote"]);
+</script>
+
+<style scoped></style>
